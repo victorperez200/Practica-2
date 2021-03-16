49ce098a505eb9b160a6bbf538329bc1d06f6156
victorperez200 <78993279+victorperez200@users.noreply.github.com>
2021-03-16 03:30:32 -0400

    Add files via upload

---
+++ ejercicio#1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ejercicio_1
+{
+    class suma
+    {
+        static void Main(string[] args)
+        {
+            int suma = 0;
+            int valor = 1;
+            int i = 0;
+
+            while (valor > 0)
+            {
+                Console.WriteLine("Entrar Valor: ");
+                valor = int.Parse(Console.ReadLine());
+
+                suma = suma + valor;
+                i++;
+            }
+            Console.WriteLine("La suma es igual a:  {0}", + suma);
+            Console.WriteLine("Total de numeros ingresados:  {0}",  + i);
+        }
+    }
+}
